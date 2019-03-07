26b79eb18697d433e6218bf5c7515fe4825f7c8a
Spencer Neese <spencer.neese@domo.com>
2019-03-07 15:46:46 -0700

    Merge branch 'use-updated-wrapper' into with-phoenix
    * use-updated-wrapper:
    Update README and add helper functions to index.js

--- src/index.js
+++ src/index.js
@@ -67,3 +67,82 @@ const options = {
 chartIt(chartType, data, options);
 //// End Sample Chart
 
+
+// Add your app code here
+
+
+
+
+
+
+/*/////////////////////////////////////////////////// 
+Below are helper functions to make querying and 
+charting data with DomoPhoenix easier
+///////////////////////////////////////////////////*/
+function getData(datasetAlias, columns){
+    // Create a query object
+    // For a full list of "query operators" see: https://developer.domo.com/docs/dev-studio-references/data-api
+    var query = {
+        "fields": getColumnNames(columns)
+    };
+
+    // Some DataSets are massive and will bring any web browser to its knees if you
+    // try to load the entire thing. To keep your app performing optimally, take
+    // advantage of filtering, aggregations, and group by's to bring down just the
+    // data your app needs. Do not include all columns in your data mapping file,
+    // just the ones you need. Setting the limit to 1000 will prevent enormous result
+    // sets while you are experimenting.
+    return domo.get(makeQueryString(datasetAlias, columns, query) + '&limit=1000');
+}
+
+function makeQueryString(datasetAlias, columns, queryObject){
+    var query = '/data/v1/' + datasetAlias + '?';
+
+    // Handle date grains
+    processGrains(columns, queryObject);
+
+    for(var key in queryObject){
+        if (queryObject.hasOwnProperty(key)) {
+            var value = queryObject[key];
+            if(typeof value === "object" && value.join != null){
+                value = value.join(",");
+            }
+            query += "&" + key + "=" + value;
+        }
+    }
+
+    return query;
+}
+
+function getColumnNames(cols){
+    return cols && cols.map(c => c.name);
+}
+
+function processGrains(cols, query) {
+    const grainColumn = cols && cols.find(c => c.dateGrain != null);
+    if(grainColumn){
+        query.dategrain = "'" + grainColumn.name + "' by " + grainColumn.dateGrain;
+        const groupBys = cols && 
+            cols.filter(c => c.type === DomoPhoenix.DATA_TYPE.STRING)
+                .map(c => `'${c.name}'`);
+
+        if(groupBys && groupBys.length > 0){
+            if(query.groupby != null){
+                typeof query.groupby === "string" && (query.groupby = query.groupby.split(","));
+                groupBys.forEach(gB => {
+                    const withoutParens = gB.replace(/'/g, "");
+                    if(query.groupby.indexOf(gB) === -1 && query.groupby.indexOf(withoutParens) === -1){
+                        query.groupby.push(gB);
+                    }
+                });
+            }
+            else{
+                query.groupby = groupBys;
+            }
+        }
+    }
+}
+
+function displayError(){
+    document.body.appendChild(document.createTextNode('Error getting data'));
+}
