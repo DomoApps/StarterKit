b173b1680e9deb8f747ccd37fdf018f0be13852b
Spencer Neese <spencer.neese@domo.com>
2019-03-07 13:17:11 -0700

    Convert helper functions to ES6

--- src/index.js
+++ src/index.js
@@ -25,8 +25,9 @@ const columns = [
         mapping: DomoPhoenix.MAPPING.SERIES
     },
     {
-        type: DomoPhoenix.DATA_TYPE.STRING,
-        name: 'Customer Segment',
+        type: DomoPhoenix.DATA_TYPE.DATE,
+        name: 'Order Date',
+        dateGrain: 'quarter',
         mapping: DomoPhoenix.MAPPING.ITEM
     },
     {
@@ -71,22 +72,23 @@ function getData(datasetAlias, columns){
         "fields": getColumnNames(columns)
     };
 
-    // Handle date grains
-    processGrains(columns, query);
-
     // Some DataSets are massive and will bring any web browser to its knees if you
     // try to load the entire thing. To keep your app performing optimally, take
     // advantage of filtering, aggregations, and group by's to bring down just the
     // data your app needs. Do not include all columns in your data mapping file,
     // just the ones you need. Setting the limit to 1000 will prevent enormous result
     // sets while you are experimenting.
-    return domo.get(makeQueryString(datasetAlias, query) + '&limit=1000');
+    return domo.get(makeQueryString(datasetAlias, columns, query) + '&limit=1000');
 }
 
 
 ////// Helper functions ////////////
-function makeQueryString(datasetAlias, queryObject){
+function makeQueryString(datasetAlias, columns, queryObject){
     var query = '/data/v1/' + datasetAlias + '?';
+
+    // Handle date grains
+    processGrains(columns, queryObject);
+
     for(var key in queryObject){
         if (queryObject.hasOwnProperty(key)) {
             var value = queryObject[key];
@@ -101,40 +103,30 @@ function makeQueryString(datasetAlias, queryObject){
 }
 
 function getColumnNames(cols){
-    var names = [];
-    for(var i in cols){
-        if(cols[i] && cols[i].name) names.push(cols[i].name);
-    }
-    return names;
+    return cols && cols.map(c => c.name);
 }
 
 function processGrains(cols, query) {
-    var grainColumn = null;
-    for(var i in cols){
-        if(cols[i] && cols[i].dateGrain != null){
-            grainColumn = cols[i];
-            break;
-        }
-    }
+    const grainColumn = cols && cols.find(c => c.dateGrain != null);
     if(grainColumn){
         query.dategrain = "'" + grainColumn.name + "' by " + grainColumn.dateGrain;
-        var groupBys = [];
-        for(var i in cols){
-            if(cols[i] && cols[i].type === DomoPhoenix.DATA_TYPE.STRING){
-                groupBys.push(cols[i] && ("'" + cols[i].name + "'"));
+        const groupBys = cols && 
+            cols.filter(c => c.type === DomoPhoenix.DATA_TYPE.STRING)
+                .map(c => `'${c.name}'`);
+
+        if(groupBys && groupBys.length > 0){
+            if(query.groupby != null){
+                typeof query.groupby === "string" && (query.groupby = query.groupby.split(","));
+                groupBys.forEach(gB => {
+                    const withoutParens = gB.replace(/'/g, "");
+                    if(query.groupby.indexOf(gB) === -1 && query.groupby.indexOf(withoutParens) === -1){
+                        query.groupby.push(gB);
+                    }
+                });
             }
-        }
-        if(query.groupby != null){
-            typeof query.groupby === "string" && (query.groupby = query.groupby.split(","));
-            for(var i in groupBys){
-                const withoutParens = groupBys[i].replace(/'/g, "");
-                if(query.groupby.indexOf(groupBys[i]) === -1 && query.groupby.indexOf(withoutParens) === -1){
-                    query.groupby.push(groupBys[i]);
-                }
+            else{
+                query.groupby = groupBys;
             }
-        }
-        else{
-            query.groupby = groupBys;
         }
     }
 }
